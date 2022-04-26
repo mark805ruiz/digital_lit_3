25f9aeb31477645ed175629f784b4053a4e75532
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-26 09:17:54 -0600

    Add files via upload

---
+++ 3_1_skill_check.py
@@ -0,0 +1,7 @@
+import random
+
+names = ['Gilberto','Christiana','Amie','Vance','Denise','Rutha','Otha','Reita','Yuriko','Loise']
+
+randomIndex = random.randint(0,len(names)-1)
+
+print(names[randomIndex])
