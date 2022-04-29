5e87cf3450241453f598542eaa2cb1c644b5ca5c
mark805ruiz <77691714+mark805ruiz@users.noreply.github.com>
2022-04-29 09:25:01 -0600

    Add files via upload

---
+++ 3_2_skill_check_2.py
@@ -0,0 +1,14 @@
+names = 'Gilberto Christiana Amie Vance Denise Rutha Otha Reita Yuriko Jacques Evelina Omar Shizuko Camellia Dede Rozanne Jerlene Loree Shanice Fredric Marietta Tandy Kelli Derek Rochelle Nichelle Nia Nathanael Jeanne Deanne Cherry Carmine Anya Tresa Isa Preston Kimberely Lorrie Elias Caryn Blanche Bunny Darron Laurel Lettie Cori Francesco Shameka Jong Loise Caitlin Thresa Francie Xuan Troy Daron Lavette Astrid Sunshine Pandora Kaye Carlena Prudence Saundra Terence Wilson Renda Mohammad Perry Fumiko Sherika Van Micki Janet Catherin Edgardo Kassie Rebbeca Cherri Pura Madison Jodie Kristie Yael Carlyn Takako Rosina Ashlee Lynetta Thomas Myrtis Kayce Dominick Tenisha Tiffiny Migdalia Sharyl Billy Oma Machelle Kiesha Xochitl Elvie Ardis Whitney Jonie Josiah Valentin Alix Aleshia Maricela Mitchell Rozanne Rosia Sonya Angelita Royce Barbra Jacklyn Evelia Katina Agatha Jaquelyn Cathy Dee Astrid Rikki Scot Sarita Catrice Darrin Brittanie Leena Jerold Kathe Rashida Lyndon Reggie Celestine Jamey Audie Brice Linda Alene Dorian Jenice Muoi Yoshiko Joi Susan'
+
+uppercase = names.upper()
+
+lowercase = names.lower()
+
+print('Do you want to print the names in uppercase or lowercase?')
+upperOrLower = input().lower()
+if upperOrLower.startswith('u'):
+    uppercase.split()
+    print(uppercase)
+else:
+    lowercase.split()
+    print(lowercase)
